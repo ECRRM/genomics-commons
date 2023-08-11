273335b2cbaf2ca5ca8f4963874159c8d1484f77
Ahmed Moustafa <amoustafa@aucegypt.edu>
2023-08-11 08:56:34 +0300

    Update build-and-publish.sh

--- build-and-publish.sh
+++ build-and-publish.sh
@@ -4,8 +4,8 @@
 # sudo docker rm -vf $(sudo docker ps -aq)
 # sudo docker rmi -f $(sudo docker images -aq)
 
-sudo docker build -t ghcr.io/ahmedmoustafa/bioinformatics-toolbox .
-# sudo docker run -v /data:/data -it ghcr.io/ahmedmoustafa/bioinformatics-toolbox /bin/bash
+sudo docker build -t ghcr.io/ECRRM/genomics-toolbox .
+# sudo docker run -v /data:/data -it ghcr.io/ECRRM/genomics-toolbox /bin/bash
 # ~/token.txt is a Github personal access tokens
-cat ~/token.txt | sudo docker login ghcr.io -u ahmedmoustafa --password-stdin
-sudo docker push ghcr.io/ahmedmoustafa/bioinformatics-toolbox
+cat ~/token.txt | sudo docker login ghcr.io -u ECRRM --password-stdin
+sudo docker push ghcr.io/ECRRM/genomics-toolbox
